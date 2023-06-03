c6ca9879ab5c802b2ddd8c6e7faccffa972042c7
myzticx <navi.juss@gmail.com>
2023-06-03 22:59:55 +0100

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,66 @@
+
 import React from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
+import './src/App';
+
+
+
+const Header = () => {
+  return (
+    <header id="header">
+      <h1>What We Do.</h1>
+      <p>
+        <span className="core-partnership">We partner with our customers across three</span>
+        <span className="core-discipline">core disciplines</span>
+        {'\u0020'.repeat(14)}
+        ㅤ
+      </p>
+    </header>
+  );
+};
+
+interface BoxProps {
+  jpgFile: string;
+  title: string;
+  description: string;
+}
+
+const Box: React.FC<BoxProps> = ({ jpgFile, title, description }) => {
+  return (
+    <div className="box">
+      <img src={jpgFile} alt={title} />
+      <p>{title}</p>
+      {'\u0020'.repeat(14)}
+      <span>{description}</span>
+    </div>
+  );
+};
+
 
 function App() {
   return (
-    <div className="box-container">
-    <div className="box"><object data="svg1.svg" type="image/svg+xml"></object><p>Thinking</p>ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<span>Unique perspectives to deliver unique outcomes. Rich perspectives and thinking which operate in new ways. Leveraging experience, research, data and insight to unlock opportunities.</span></div>
-    <div className="box"><object data="svg2.svg" type="image/svg+xml"></object><p>Design</p>ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<span>Every journey begins with a single step. We’re iterating endlessly in Creative and Design, Product Experience, Customer Insights, UX and Service Design</span></div>
-    <div className="box"><object data="svg3.svg" type="image/svg+xml"></object><p>Delivery</p>ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<span>Building new & optimising existing products & services. Harnessing the power of MACH architecture to accelerate delivery daily and achieve outcomes every 60 days</span></div>
-  </div>
+    <div>
+      <Header />
+      <div className="box-container">
+        <Box
+          jpgFile="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/61a7a12c7cb1e59f7ee4d34d_noun_Brain_2998851.svg"
+      
+          title="Thinking"
+          description="Unique perspectives to deliver unique outcomes. Rich perspectives and thinking which operate in new ways. Leveraging experience, research, data and insight to unlock opportunities."
+        />
+        <Box
+          jpgFile="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/61a7a12c7687e256955b5d90_noun_Technology_2626059%20(4).svg"
+          title="Design"
+          description="Designing and creating solutions that are intuitive, effective and delightful. Understanding the user and what they need, to create the best possible experience."
+        />
+        <Box
+          jpgFile="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/61a7a12c2e49a6801005e409_noun_supply_4133986%20(2).svg"
+          title="Delivery"
+          description="Delivering quality outcomes, on time and on budget. Ensuring that our solutions are reliable, scalable and easy to maintain. Continuously improving and innovating to deliver the best possible outcomes."
+        />
+      </div>
+    </div>
   );
 }
 
