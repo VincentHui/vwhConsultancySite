e0728121a20b10592aaa6ef5f282c3352dcfec00
myzticx <navi.juss@gmail.com>
2023-06-03 13:57:37 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="box-container">
+    <div className="box"><object data="svg1.svg" type="image/svg+xml"></object><p>Thinking</p>ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<span>Unique perspectives to deliver unique outcomes. Rich perspectives and thinking which operate in new ways. Leveraging experience, research, data and insight to unlock opportunities.</span></div>
+    <div className="box"><object data="svg2.svg" type="image/svg+xml"></object><p>Design</p>ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<span>Every journey begins with a single step. We’re iterating endlessly in Creative and Design, Product Experience, Customer Insights, UX and Service Design</span></div>
+    <div className="box"><object data="svg3.svg" type="image/svg+xml"></object><p>Delivery</p>ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ<span>Building new & optimising existing products & services. Harnessing the power of MACH architecture to accelerate delivery daily and achieve outcomes every 60 days</span></div>
+  </div>
   );
 }
 
