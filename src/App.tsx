19c185c66bdde08de354c7e144448f1d88663e9f
myzticx <navi.juss@gmail.com>
2023-07-05 21:31:08 +0100

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { WhatWeDoSection } from "./sections/WhatWeDo";
 import WhoAreWeSection from "./sections/WhoAreWe";
+import ContactUsSection from "./sections/ContactUs";
 
 function App() {
   return (
     <div>
       <WhoAreWeSection />
       <WhatWeDoSection />
+      <ContactUsSection children={undefined} />
     </div>
   );
 }
-
+//
 export default App;
