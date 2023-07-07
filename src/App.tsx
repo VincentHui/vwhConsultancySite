f76932ed31085f66092dbe8e9b90463e9f5aec5e
Vincent-eyekandy <vincent@eyekandy.com>
2023-07-07 20:07:04 +0100

    adding corrections and adding to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { WhatWeDoSection } from "./sections/WhatWeDo";
 import { WhoAreWeSection } from "./sections/WhoAreWe";
+import { AboutUs } from "./sections/AboutUs";
 
 function App() {
   return (
     <div>
       <WhoAreWeSection />
+      <AboutUs />
       <WhatWeDoSection />
     </div>
   );
--- src/sections/AboutUs.tsx
+++ src/sections/AboutUs.tsx
@@ -1,43 +1,9 @@
-import "./App.css";
+import "./AboutUs.css";
 
-function Footer() {
+export function AboutUs() {
   return (
-    <div className="fs-cc-components">
-      <div fs-cc="banner" className="fs-cc-banner_component">
-        <div className="fs-cc-banner_container">
-          <div className="fs-cc-banner_buttons-wrapper"></div>
-        </div>
-        <div fs-cc="interaction" className="fs-cc-banner_trigger"></div>
-      </div>
-    </div>
-  );
-}
-
-function App() {
-  return (
-    <div className="body">
-      <div
-        data-w-id="70f478b7-63de-7ab7-d587-46a760f56c36"
-        className="preloader"
-        data-ix="preloader"
-      >
-        <img
-          src="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/5f679f642baf7ce2c3eab5c2_oval.svg"
-          width="50"
-          alt=""
-        />
-      </div>
-      <div
-        data-poster-url="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/5f67a32f01a9196fba1e0c50_WebRes-poster-00001.jpg"
-        data-video-urls="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/5f67a32f01a9196fba1e0c50_WebRes-transcode.mp4,https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/5f67a32f01a9196fba1e0c50_WebRes-transcode.webm"
-        data-autoplay="true"
-        data-loop="true"
-        data-wf-ignore="true"
-        id="hero-video"
-        data-beta-bgvideo-upgrade="false"
-        className="hero-home w-background-video w-background-video-atom"
-      ></div>
-      <div data-w-id="1ed91721-7108-eb29-28b5-6002547fc472">
+    <div>
+      <div>
         <div className="yellow-line wf-section"></div>
         <div id="about" className="about-section wf-section">
           <div className="image-text-col-wrapper">
@@ -73,18 +39,12 @@ function App() {
             </div>
             <img
               src="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/61a75c02e4ee5215811d7409_About%20Us.jpg"
-              loading="lazy"
-              sizes="(max-width: 666px) 90vw, 600px"
-              srcset="https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/61a75c02e4ee5215811d7409_About%20Us-p-500.jpeg 500w, https://uploads-ssl.webflow.com/5f679f64b6be43a8f3af1324/61a75c02e4ee5215811d7409_About%20Us.jpg 600w"
               alt="two men talking and looking at a mobile tablet"
               className="rounded-image"
             />
           </div>
         </div>
       </div>
-      <Footer />
     </div>
   );
 }
-
-export default App;
