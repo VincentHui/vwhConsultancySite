9430122226010f144c6e557e0b7a9cfbd41f10ca
vince <vince>
2023-07-08 14:50:23 +0100

    crazycrazy

--- src/sections/WorkWithUs.tsx
+++ src/sections/WorkWithUs.tsx
@@ -1,4 +1,27 @@
 import React from "react";
 import "./WorkWithUs.css";
 
+function Header() {
+  return (
+    <header>
+      <p>WORK WITH US</p>
+      <p>
+        <div className="Workwithus">
+          <div className="strategicthinkers">
+            <div className="deliveryleads">
+              <div className="techleads">
+                <div className="productmanagers">
+                  <div className="softwareengineers">
+                    <div className="UX/UIDesigners"></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </p>
+    </header>
+  );
+}
+
 export const WorkWithUsSection = () => <p>"why does this happen"</p>;
