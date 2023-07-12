0b901909ba2dd6a3770f3b19c5507dbe2b5245ac
msrathoure <singhmanmohn87@gmail.com>
2023-07-12 23:02:53 +0100

    help me

--- src/sections/WorkWithUs.tsx
+++ src/sections/WorkWithUs.tsx
@@ -1,27 +1,49 @@
 import React from "react";
 import "./WorkWithUs.css";
 
-function Header() {
+function WorkWithUs() {}
+
+
+export const WorkWithUsSection =() => {
   return (
-    <header>
-      <p>WORK WITH US</p>
-      <p>
-        <div className="Workwithus">
-          <div className="strategicthinkers">
-            <div className="deliveryleads">
-              <div className="techleads">
-                <div className="productmanagers">
-                  <div className="softwareengineers">
-                    <div className="UX/UIDesigners"></div>
+    <header id="header">
+      <div className="work-with-us-section" id="work-with-us">
+        <span className="centered-container-small">WORK WITH US
+          <span className="center-text">
+          <p>we are growing our digital consulting & delivery organisation and we're always keen to work with new people.</p>
+          </span>
+          <span className="section-text">
+            we'd love to hear from you if you think you can help us rethink how things are done and create a new whitespace.
+            <br>
+            </br>we are looking for:
+          </span>
+          <div className="w-layout-grid roles-grid">
+          <div id="w-node-_790fd235-7fa1-c797-1cf0-33d083ee4326-07eab589" className="roles-card">
+            <div className="roles-text">Strategic Thinkers</div>
+            <div id="w-node-a49ff584-224f-b37c-6368-aed4f7897c7d-07eab589" className="roles-card">
+              <div className="roles-text"> Delivery Leads</div>
+              <div id="w-node-_6309550d-6512-f1fe-2ef9-b2045b3ed8c6-07eab589" className="roles-card">
+              <div className="roles-text">Tech Leads</div>
+              <div id="w-node-_8d31b421-05ac-d123-8617-91fd88639660-07eab589" className="roles-card">
+              <div className="roles-text">Product Managers</div>
+              <div id="w-node-_7dee52e9-cb06-86ff-0747-4f4ec538738b-07eab589" className="roles-card">
+                <div className="roles-text">Software Engineers</div>
+                <div id="w-node-cf7668f7-0014-872c-51c1-0b5a5a7d91ca-07eab589" className="roles-card">
+                  <div className="roles-text">UX/UI Designers</div>
                   </div>
-                </div>
+                  <a href="https://careers.modu.digital/" target="_blank" className="button-2 yellow blue w-button">Find out more
+                  </a>
+              </div>
+              </div>
               </div>
             </div>
           </div>
-        </div>
-      </p>
+          </div>
+        </span>
+      </div>
     </header>
-  );
+  )
 }
 
-export const WorkWithUsSection = () => <p>"why does this happen"</p>;
+
+
